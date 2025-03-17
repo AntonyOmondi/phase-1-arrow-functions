68dd44f0a53621e72fcab12621189ed158d68682
Antony Omondi <tonymboya13@gmail.com>
2025-03-17 18:11:48 +0300

    complete arrow-functions lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function (num1, num2){
+    return num1 / num2;
+}
+divide(2000, 1000)
+
+const square = num => num ** num;
+square(2)
+
+const add = (num1, num2) => num1 + num2;
+add(1,1)
